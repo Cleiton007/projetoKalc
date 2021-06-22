6beec4e74fafc1f1e0a48fa18e52ef420c897ab1
Cleiton007 <51724245+Cleiton007@users.noreply.github.com>
2021-06-22 14:58:43 -0300

    Add files via upload

---
+++ projetoKalc-main/js/kalcWeb.js
@@ -0,0 +1,95 @@
+console.log("Teste calculadora");
+
+//Operações
+const adicao = document.getElementById('adicao');
+const subtracao = document.getElementById('subtracao');
+const multiplicacao = document.getElementById('multiplicacao');
+const divisao = document.getElementById('divisao');
+const igual = document.getElementById('igual');
+
+//Números
+const um = document.getElementById('um');
+const dois = document.getElementById('dois');
+const tres = document.getElementById('tres');
+const quatro = document.getElementById('quatro');
+const cinco = document.getElementById('cinco');
+const seis = document.getElementById('seis');
+const sete = document.getElementById('sete');
+const oito = document.getElementById('oito');
+const nove = document.getElementById('nove');
+const zero = document.getElementById('zero');
+const ponto = document.getElementById('ponto');
+const clear = document.getElementById('clear');
+
+//Eventos numeros
+um.onclick = function(){
+    console.log(um.innerText);
+};
+
+dois.onclick = function(){
+    console.log(dois.innerText);
+};
+
+tres.onclick = function(){
+    console.log(tres.innerText);
+};
+
+quatro.onclick = function(){
+    console.log(quatro.innerText);
+};
+
+cinco.onclick = function(){
+    console.log(cinco.innerText);
+};
+
+seis.onclick = function(){
+    console.log(seis.innerText);
+};
+
+sete.onclick = function(){
+    console.log(sete.innerText);
+};
+
+oito.onclick = function(){
+    console.log(oito.innerText);
+};
+
+nove.onclick = function(){
+    console.log(nove.innerText);
+};
+
+zero.onclick = function(){
+    console.log(zero.innerText);
+};
+
+ponto.onclick = function(){
+    console.log(ponto.innerText);
+};
+
+clear.onclick = function(){
+    console.log(clear.innerText);
+};
+
+//Eventos operators
+adicao.onclick = function(){
+    console.log(adicao.innerText);
+};
+
+subtracao.onclick = function(){
+    console.log(subtracao.innerText);
+};
+
+multiplicacao.onclick = function(){
+    console.log(multiplicacao.innerText);
+};
+
+divisao.onclick = function(){
+    console.log(divisao.innerText);
+};
+
+igual.onclick = function(){
+    console.log(igual.innerText);
+};
+
+
+
